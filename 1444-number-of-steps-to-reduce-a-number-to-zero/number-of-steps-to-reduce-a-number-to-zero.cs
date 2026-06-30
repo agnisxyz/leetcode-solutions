a8d7425b0c1d78ca9c95185a89c958707dbb623d
Agnis <agnisxyz@gmail.com>
2026-06-30 16:53:42 +0300

    Time: 0 ms (100.00%) | Memory: 29.1 MB (15.53%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int NumberOfSteps(int num) {
+        int steps = 0;
+        while (num >0)
+        {
+            if(num % 2 == 0){
+                num = num/2;
+                steps++;
+            }else{
+                num = num - 1;
+                steps++;
+            }
+        }
+        return steps;
+        
+    }
+}
